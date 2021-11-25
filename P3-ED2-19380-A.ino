5c0c019b7c92ef38692adb2f02899698360a1c18
Jose031 <90293076+Jose031@users.noreply.github.com>
2021-11-25 02:24:36 -0600

    Inicializaciones
    Inicialización del sensor y del neopixel.

--- P3-ED2-19380-A.ino
+++ P3-ED2-19380-A.ino
@@ -60,6 +60,33 @@ void setup()
 {
   Serial.begin(115200); // Velocidad con la que se trabja la comunicación serial del canal 0.
   Serial2.begin(115200);  // Velocidad con la que se trabaja la comunicación serial del canal 2.
+
+  //Inicializar Neopixel
+  pixels.begin();
+  pixels.setBrightness(40);
+  //*********************************Configuración Sensor*********************************
+  pinMode(pulseLED, OUTPUT);
+  pinMode(readLED, OUTPUT);
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
+  }
+
+  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+  while (Serial.available() == 0) ; //wait until user presses a key
+  Serial.read();
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  //*****************************************************************************************
+}
 //**********************************************************************************************************************
 // LOOP
 //**********************************************************************************************************************
