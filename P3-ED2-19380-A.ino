bd38c3165d95a792553ffa4ad49f1411170f8226
Jose031 <90293076+Jose031@users.noreply.github.com>
2021-11-25 02:21:55 -0600

    Definición de variables globales

--- P3-ED2-19380-A.ino
+++ P3-ED2-19380-A.ino
@@ -33,6 +33,26 @@ void Neopixel3(void); //Prototipo de funcion que se encarga de encender el neopi
 //**********************************************************************************************************************
 // Variables Globales
 //**********************************************************************************************************************
+String estadoBoton = ""; // Variable con la cual se recibe la instrucción por parte de la tiva c para enviar el valor BPM y SPO2.
+int estado = 0;
+//Variables globales para el funcionamiento del sensor
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+byte pulseLED = 11; //Must be on PWM pin
+byte readLED = 13; //Blinks with each data read
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 //**********************************************************************************************************************
 // Configuracion
 //**********************************************************************************************************************
